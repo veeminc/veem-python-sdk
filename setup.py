3171cc04492335b77c75b1efeb367a94f935d02e
dharsomsubhro <sodhar@ucsd.edu>
2018-09-20 15:45:23 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from distutils.command.register import register as register_orig
+from distutils.command.upload import upload as upload_orig
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "Veem-python"
+VERSION = "1.0.3"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+class register(register_orig):
+
+    def _get_rc_file(self):
+        return os.path.join('.', '.pypirc')
+
+class upload(upload_orig):
+
+    def _get_rc_file(self):
+        return os.path.join('.', '.pypirc')
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Veem Python API",
+    author_email="dev@veem.com",
+    url="https://github.com/dharsomsubhro/veem-python-new/archive/1.0.2.tar.gz",
+    keywords=["Veem API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    Veem REST API
+    """
+)
